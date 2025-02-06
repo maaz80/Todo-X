dd2af19a5af7230de26d68cd30439d58546034fe
maaz-shakeel0 <maazshakeel56@gmail.com>
2025-02-06 22:18:51 +0530

    Added comment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
     return (
+        // Navbar 
         <nav className='w-full'>
             <ul className="logo flex gap-6 p-3 text-white bg-slate-600 justify-between shadow-2xl">
                 <div className=''>
                     <li className=' text-base font-bold font-serif'>Todo-X</li>
                 </div>
                 <div className="con flex gap-5">
-                <li className='cursor-pointer hover:font-bold transition-all text-[14px]'>Home</li>
-                <li className='cursor-pointer hover:font-bold transition-all text-[14px]'>About</li>
-                <li className='cursor-pointer hover:font-bold transition-all text-[14px]'>Contact Us</li>
+                    <li className='cursor-pointer hover:font-bold transition-all text-[14px]'>Home</li>
+                    <li className='cursor-pointer hover:font-bold transition-all text-[14px]'>About</li>
+                    <li className='cursor-pointer hover:font-bold transition-all text-[14px]'>Contact Us</li>
                 </div>
             </ul>
         </nav>
